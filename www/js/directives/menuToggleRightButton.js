73892c3382a8e6d08e2e6eea8027c374232aee2e
Manoel Quirino Neto <manoelifpb@gmail.com>
2016-02-29 17:30:54 -0300

    logout button

--- www/js/directives/menuToggleRightButton.js
+++ www/js/directives/menuToggleRightButton.js
@@ -1,6 +1,13 @@
-app.directive('menuToggleRightButton', function() {
-  return {
-    templateUrl: 'templates/directives/menu-toggle-right-button.html',
-    link: function(scope, el, attrs) {}
-  };
-});
+app.directive('menuToggleRightButton', [
+  'User', '$state', function(User, $state) {
+    return {
+      templateUrl: 'templates/directives/menu-toggle-right-button.html',
+      link: function(scope, el, attrs) {
+        return scope.logout = function() {
+          User.current_user = null;
+          return $state.go('login');
+        };
+      }
+    };
+  }
+]);
